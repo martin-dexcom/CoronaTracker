8fdbd453203f6def8b98345135bfcf77c16d2718
Jessica Anguiano <jessica.anguiano@dexcom.com>
2021-10-14 13:43:56 -0700

    changing branches

--- CoronaTracker/CoronaTracker/View/Countries/CountriesListView.swift
+++ CoronaTracker/CoronaTracker/View/Countries/CountriesListView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 
+
 struct CountriesListView: View {
     let countries : [Country]
     
---
+++ CoronaTracker/CoronaTracker/View/CountryStatsBoxView.swift
@@ -0,0 +1,20 @@
+//
+//  CountryStatsBoxView.swift
+//  CoronaTracker
+//
+//  Created by Jessica Anguiano on 10/14/21.
+//
+
+import SwiftUI
+
+struct CountryStatsBoxView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CountryStatsBoxView_Previews: PreviewProvider {
+    static var previews: some View {
+        CountryStatsBoxView()
+    }
+}
